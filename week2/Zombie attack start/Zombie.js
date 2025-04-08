4e1ef1be62c8e1c6926c8e3ad4729c9c70cc145d
Emils Berzs <emilsberzs@hotmail.com>
2025-04-08 21:34:28 +0100

    Added health

---
+++ week2/Zombie attack start/Zombie.js
@@ -0,0 +1,35 @@
+//constructor for the Zombies
+function zombie(y) {
+	//initial x so all zombies start to the left of the screen
+	this.x = -10;
+	this.y = y;
+	//set a random speed
+	this.speed = random(0.2, 0.5);
+    //give each zombie health property
+    this.health = 10;
+
+	//draw the zombie to the screen
+	this.draw = function() {
+		push();
+		//arms
+		fill(128, 0, 128);
+		rect(this.x - 10, this.y - 50, 65, 15);
+		rect(this.x - 10, this.y + 35, 65, 15);
+		//shoulders
+		rect(this.x - 20, this.y - 50, 35, 100, 10);
+		//head
+		fill(50);
+		ellipse(this.x, this.y, 50);
+        //health
+        fill(255,255,255)
+        text(this.health,this.x,this.y);
+		pop();
+	}
+
+      //update the zombies x location as it lumbers across the screen
+	this.updateLocation = function() {
+		this.x += this.speed;
+	}
+
+    
+}
--- week2/Zombie attack start/sketch.js
+++ week2/Zombie attack start/sketch.js
@@ -4,7 +4,7 @@ Hack it: Zombie Attack
 The program creates a horde of zombies that cross the canvas. Read the code
 and make the following enhancements
 
-1. Split the code up into muliple files one for each constructor
+*1. Split the code up into muliple files one for each constructor
 2. Give each zombie a health property
 3. Add a shovel! When the zombie is clicked on the head their health decreases
 	* Add a 'clicked' method to the zombie to check if x and y coordinates, taken as arguments
@@ -25,62 +25,27 @@ function setup() {
 
 	//create a new horde and add zombies
 	horde = new Horde();
-	horde.addZombies(7);
+	horde.addZombies(1);
 
+}
+
+///GET IT WORKING!!!!
+function mouseClicked(){
+	console.log('click')
+	for (var i = 0; i<horde.length; i++){
+		if(dist(mouseX,mouseY,horde.zombies[i].x,horde.zombies[i].y)<=100){
+			horde.zombies[i].health =0
+			console.log(horde.zombies[i].x)
+		}
+	}
 }
 
 function draw() {
+	
 	background(77, 112, 107);
 	this.horde.drawZombies();
+	
 
 }
 
-//Constructor for the horde
-function Horde() {
-	//an array of zombies
-	this.zombies = [];
 
-	//call each zombies drawing code and update it's location ready to be drawn again
-	this.drawZombies = function() {
-		for (var i = 0; i < this.zombies.length; i++) {
-			this.zombies[i].draw();
-			this.zombies[i].updateLocation();
-		}
-	}
-
-	//add n zombies to the horde
-	this.addZombies = function(n) {
-		for (var i = 0; i < n; i++) {
-			this.zombies.push(new zombie(random(20, height - 50)))
-		}
-	}
-}
-
-//constructor for the Zombies
-function zombie(y) {
-	//initial x so all zombies start to the left of the screen
-	this.x = -10;
-	this.y = y;
-	//set a random speed
-	this.speed = random(0.2, 0.5);
-
-	//draw the zombie to the screen
-	this.draw = function() {
-		push();
-		//arms
-		fill(128, 0, 128);
-		rect(this.x - 10, this.y - 50, 65, 15);
-		rect(this.x - 10, this.y + 35, 65, 15);
-		//shoulders
-		rect(this.x - 20, this.y - 50, 35, 100, 10);
-		//head
-		fill(50);
-		ellipse(this.x, this.y, 50);
-		pop();
-	}
-
-	//update the zombies x location as it lumbers across the screen
-	this.updateLocation = function() {
-		this.x += this.speed;
-	}
-}
