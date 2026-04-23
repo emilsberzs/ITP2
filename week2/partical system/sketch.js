43e13766bc7e7e562f95a20dd491cfef91a29f1d
Emils Berzs <emilsberzs@hotmail.com>
2026-04-23 12:11:14 +0100

    Week 2 finished

--- week2/partical system/sketch.js
+++ week2/partical system/sketch.js
@@ -2,15 +2,13 @@
 
 var emit;
 
-function setup()
-{
+function setup() {
 	createCanvas(800, 600);
-	emit = new Emmitter(width / 2, height - 100, 0, -1.5, 20, color(200, 0, 100, 100));
-	emit.startEmitter(10, 2000);
+	emit = new Emmitter(width / 2, height/2, 0, -0.8, 20, color(100, 0, 200, 100));
+	emit.startEmitter(20, 20000);
 }
 
-function draw()
-{
+function draw() {
 	background(10);
 	emit.drawParticles();
 }
